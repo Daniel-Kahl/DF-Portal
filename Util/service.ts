66e50eb39d084f1002f747268c4863d01d834037
NGoffi <92411234+NGoffi@users.noreply.github.com>
2023-08-10 17:57:35 -0500

    Work in progress getEquipment

--- Util/service.ts
+++ Util/service.ts
@@ -2,23 +2,128 @@ const SERVER = "cain";
 const API_URL = "https://api.dfoneople.com/df/" + SERVER;
 const API_KEY = 'apikey=' + process.env.API_KEY;
 
-export interface Characters {
-  rows: Row[]
+export interface Character {
+  rows: CharacterRow[]
 }
 
-export interface Row {
-  serverId: string
-  characterId: string
-  characterName: string
-  level: number
-  jobId: string
-  jobGrowId: string
-  jobName: string
-  jobGrowName: string
+export interface CharacterRow {
+  serverId: string,
+  characterId: string,
+  characterName: string,
+  level: number,
+  jobId: string,
+  jobGrowId: string,
+  jobName: string,
+  jobGrowName: string,
   fame: number
 }
 
-export async function getCharacter(charName: string): Promise<Characters> {
+export interface Equppied {
+  rows: EquppiedRow[]
+}
+
+export interface EquppiedRow {
+  characterId: string,
+  characterName: string,
+  level: number,
+  jobId: string,
+  jobGrowId: string,
+  jobName: string,
+  jobGrowName: string,
+  adventureName: string,
+  guildId: string,
+  guildName: string,
+  equipment: Equipment[],
+  setItemInfo: any[]
+}
+
+export interface Equipment {
+  slotId: string
+  slotName: string
+  itemId: string
+  itemName: string
+  itemTypeId: string
+  itemType: string
+  itemTypeDetailId: string
+  itemTypeDetail: string
+  itemAvailableLevel: number
+  itemRarity: string
+  setItemId: any
+  setItemName: any
+  reinforce: number
+  itemGradeName: string
+  enchant: Enchant
+  amplificationName: string
+  refine: number
+  growInfo: GrowInfo
+  engraveName: boolean
+  machineRevolutionInfo: MachineRevolutionInfo
+  upgradeInfo: UpgradeInfo
+  ispinsInfo: IspinsInfo
+}
+
+export interface Enchant {
+  status: Status[]
+}
+
+export interface Status {
+  name: string
+  value: any
+}
+
+export interface GrowInfo {
+  transfer: boolean
+  total: Total
+  options: Option[]
+}
+
+export interface Total {
+  damage: number
+  buff: number
+  level: number
+}
+
+export interface Option {
+  level: number
+  expRate: number
+  explain: string
+  explainDetail: string
+  damage: number
+  default: Default
+  buff: number
+}
+
+export interface Default {
+  damage: number
+  buff: number
+}
+
+export interface MachineRevolutionInfo {
+  options: Option2[]
+}
+
+export interface Option2 {
+  buff: number
+  explain: string
+  explainDetail: string
+}
+
+export interface UpgradeInfo {
+  itemId: string
+  itemName: string
+}
+
+export interface IspinsInfo {
+  options: Option3[]
+}
+
+export interface Option3 {
+  buff: number
+  explain: string
+  explainDetail: string
+}
+
+export async function getCharacter(charName: string): Promise<Character> {
 
     const headers: Headers = new Headers()
     headers.set('Content-Type', 'application/json')
@@ -26,17 +131,38 @@ export async function getCharacter(charName: string): Promise<Characters> {
   
     // Create the request object, which will be a RequestInfo type. 
     // Here, we will pass in the URL as well as the options object as parameters.
-    const request: RequestInfo = new Request("https://api.dfoneople.com/df/servers/cain/characters?characterName=" + charName + "&wordType=match&" + API_KEY, {
+    const request: RequestInfo = new Request("https://api.dfoneople.com/df/servers/cain/characters?characterName=" + charName + "&wordType=match&"+API_KEY, {
       method: 'GET',
       headers: headers
     })
-    
-    // Use fetch to retrieve data
+
     const res = await fetch(request);
 
     if (!res.ok) {
       throw new Error('Failed to fetch data');
     }
+    // Use fetch to retrieve data 
+    return res.json();
+  }
+
+  export async function getEquipment(charID: string): Promise<Equppied> {
+
+    const headers: Headers = new Headers()
+    headers.set('Content-Type', 'application/json')
+    headers.set('Accept', 'application/json')
   
+    // Create the request object, which will be a RequestInfo type. 
+    // Here, we will pass in the URL as well as the options object as parameters.
+    const request: RequestInfo = new Request("https://api.dfoneople.com/df/servers/cain/characters/"+ charID +"/equip/equipment?" +API_KEY, {
+      method: 'GET',
+      headers: headers
+    })
+
+    const res = await fetch(request);
+
+    if (!res.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    // Use fetch to retrieve data 
     return res.json();
   }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import Image from 'next/image'
-import {getCharacter} from '../../Util/service'
+import {getCharacter, getEquipment} from '../../Util/service'
 
 export default async function Home() {
 
   const res = await getCharacter("EndOfSky");
+  const res1 = await getEquipment("224ab853b8077fdbd86844a3c8fec51d");
   const char = res.rows[0];
+  const equip = res1.rows[0];
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {char.characterName}
+      {char.jobGrowName}
+      {equip.guildName}
     </main>
   )
 }
