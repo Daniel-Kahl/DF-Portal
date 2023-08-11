774ce455d72f70f5a908bc7c9ea769a9fdcc721d
Brett Carney <brett@brettcarney.com>
2023-08-10 23:48:48 -0500

    Implemented debouncing

--- src/components/dfo_search.tsx
+++ src/components/dfo_search.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TextInput } from "flowbite-react";
 import { HiOutlineSearch } from 'react-icons/hi';
 import { Characters } from '../../Util/service';
@@ -7,20 +7,42 @@ import { useRouter } from 'next/navigation';
 
 export default function DFOSearch() {
     const { push } = useRouter();
-    const [suggestions, setSuggestions] = useState<Characters>();
+    const [suggestions, setSuggestions] = useState<Characters>({ rows: [] });
+    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState<string>('');
 
-    const handleInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
-        const text = event.target.value;
-        const response = await fetch(`/api/character?username=${text}&requestType=full`);
-        const data = await response.json();
-        setSuggestions(data);
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedSearchTerm(searchTerm);
+        }, 500);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [searchTerm]);
+
+    useEffect(() => {
+        const fetchSuggestions = async () => {
+            const response = await fetch(`/api/character?username=${debouncedSearchTerm}&requestType=full`);
+            const data = await response.json();
+            setSuggestions(data);
+        };
+
+        if (debouncedSearchTerm) {
+            fetchSuggestions();
+        }
+    }, [debouncedSearchTerm]);
+
+    const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+        setSuggestions({ rows: [] }); // clear suggestions when input changes
     }
 
     const handleSearch = async (name: string) => {
         const response = await fetch(`/api/character?username=${name}&requestType=match`);
         const data = await response.json();
         setSuggestions(data);
-        
+
         const input = document.getElementById("search") as HTMLInputElement;
         input.value = name;
 
@@ -38,16 +60,18 @@ export default function DFOSearch() {
                 onInput={handleInput}
                 className="text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-700 focus:border-blue-500 focus:ring-blue-500"
             />
-            <ul className="mt-1 rounded-md divide-y divide-gray-200 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
-                {suggestions?.rows?.length !== undefined && suggestions.rows.length > 0 && (
-                    suggestions.rows.map((suggestion, index) => (
-                        <li key={index} className="px-2 py-2 cursor-pointer hover:bg-gray-100" onClick={() => handleSearch(suggestion.characterName)}>
-                            <span className="block font-medium text-gray-900">{suggestion.characterName}</span>
-                            <span className="block text-sm text-gray-500">Level: {suggestion.level}</span>
-                        </li>
-                    ))
-                )}
-            </ul>
+            {searchTerm && ( // only render suggestions if search bar is not empty
+                <ul className="mt-1 rounded-md divide-y divide-gray-200 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                    {suggestions?.rows?.length !== undefined && suggestions.rows.length > 0 && (
+                        suggestions.rows.map((suggestion, index) => (
+                            <li key={index} className="px-2 py-2 cursor-pointer hover:bg-gray-100" onClick={() => handleSearch(suggestion.characterName)}>
+                                <span className="block font-medium text-gray-900">{suggestion.characterName}</span>
+                                <span className="block text-sm text-gray-500">Level: {suggestion.level}</span>
+                            </li>
+                        ))
+                    )}
+                </ul>
+            )}
         </div>
     );
 }
