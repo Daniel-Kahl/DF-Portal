718be2a7773e8b2738265282562e9b7d37b7e914
Jason Logerquist <18403017+jay101pk@users.noreply.github.com>
2023-08-11 01:10:47 -0500

    Add a simple landing page that prints the basic character information

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           </div>
           <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
             <div className="text-sm lg:flex-grow">
-              <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+              <a href='/' className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                 Home
               </a>
               
