b987585d39481d56fd33a4ee9369d942df2aa595
Brett Carney <brett@brettcarney.com>
2023-08-10 21:00:00 -0500

    Search bar functionality without character page.

---
+++ src/components/dfo_search.tsx
@@ -0,0 +1,53 @@
+"use client"
+import { useState } from 'react';
+import { TextInput } from "flowbite-react";
+import { HiOutlineSearch } from 'react-icons/hi';
+import { Characters } from '../../Util/service';
+import { useRouter } from 'next/navigation';
+
+export default function DFOSearch() {
+    const { push } = useRouter();
+    const [suggestions, setSuggestions] = useState<Characters>();
+
+    const handleInput = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        const text = event.target.value;
+        const response = await fetch(`/api/character?username=${text}&requestType=full`);
+        const data = await response.json();
+        setSuggestions(data);
+    }
+
+    const handleSearch = async (name: string) => {
+        const response = await fetch(`/api/character?username=${name}&requestType=match`);
+        const data = await response.json();
+        setSuggestions(data);
+        
+        const input = document.getElementById("search") as HTMLInputElement;
+        input.value = name;
+
+        push(`/character/${data.rows[0].characterId}`);
+    }
+
+    return (
+        <div className="w-11/12 mx-auto">
+            <TextInput
+                icon={HiOutlineSearch}
+                id="search"
+                placeholder="Search by username"
+                required
+                type="search"
+                onInput={handleInput}
+                className="text-gray-900 dark:text-gray-100 border-gray-300 dark:border-gray-700 focus:border-blue-500 focus:ring-blue-500"
+            />
+            <ul className="mt-1 rounded-md divide-y divide-gray-200 bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-100">
+                {suggestions?.rows?.length !== undefined && suggestions.rows.length > 0 && (
+                    suggestions.rows.map((suggestion, index) => (
+                        <li key={index} className="px-2 py-2 cursor-pointer hover:bg-gray-100" onClick={() => handleSearch(suggestion.characterName)}>
+                            <span className="block font-medium text-gray-900">{suggestion.characterName}</span>
+                            <span className="block text-sm text-gray-500">Level: {suggestion.level}</span>
+                        </li>
+                    ))
+                )}
+            </ul>
+        </div>
+    );
+}
