6ee8e473abc2f84b044db31b5d38a63835aa207b
NGoffi <92411234+NGoffi@users.noreply.github.com>
2023-08-12 01:06:49 -0500

    added equipment routing and added equipment loadout

---
+++ pages/api/equipment/[id].ts
@@ -0,0 +1,17 @@
+import {NextApiRequest, NextApiResponse} from "next";
+import {Equipped, getEquipment} from "../../../Util/service";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Equipped>
+) {
+    const { query } = req
+    const { id } = query
+
+    if (id == undefined) {
+        return res.status(401);
+    }
+
+    const data = await getEquipment(id as string);
+    return res.status(200).json(data);
+}
--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {CharacterBasicInfo, Characters, getBasicCharacterInfo, getCharacter} from '../../../../Util/service';
+import {CharacterBasicInfo, Characters, getBasicCharacterInfo, getCharacter, getEquipment,Equipped,Equipment,Enchant,Status,GrowInfo,Total,Option,Default,MachineRevolutionInfo,Option2,UpgradeInfo,IspinsInfo,Option3} from '../../../../Util/service';
 import {useEffect, useState} from "react";
 
 export default function Page({params}: { params: { id: string } }) {
@@ -14,8 +14,9 @@ export default function Page({params}: { params: { id: string } }) {
         setCharacter(characterBasicInfo);
     }
 
-    useEffect(() => {
+    useEffect(() => { //runs once on page loadup, add fetch methods here
         fetchCharacterBasicInfo();
+        fetchEquipped();
     }, [])
 
     const rows = [];
@@ -24,6 +25,17 @@ export default function Page({params}: { params: { id: string } }) {
         // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
         rows.push(<div className="h-7 rounded-full bg-gray-700 w-1/2 mb-1"></div>);
     }
+
+    const [equip, setEquip] = useState<Equipped>();
+
+    const fetchEquipped = async () => {
+        const equippedResponse = await fetch(`/api/equipment/${params.id}`);
+        const equipped : Equipped = await equippedResponse.json();
+        console.log(equipped);
+        setLoading(false);
+        setEquip(equipped);
+    }
+
 
     return (<div className="container mx-auto max-w-2xl m-10">
         <div className="text-2xl p-5">Basic Character Information</div>
@@ -52,6 +64,24 @@ export default function Page({params}: { params: { id: string } }) {
             </div>
             <div>
                 Guild Name: {character.guildName}
+            </div>
+        </div>
+        }
+
+        <div className="text-2xl p-5">Equipment</div>
+        {loading || equip == undefined ? <div>
+            <div role="status" className="animate-pulse mb-5">
+                {rows}
+                <span className="sr-only">Loading...</span>
+            </div>
+
+        </div> : <div className="p-5">
+            <div>
+            {equip.equipment.map((equip, index) => (
+                              <div key={index} className="px-2 py-2 bg-slate-500">
+                                {`${equip.slotName}:     ${equip.itemName} `} 
+                              </div>
+                          ))}
             </div>
         </div>
         }
