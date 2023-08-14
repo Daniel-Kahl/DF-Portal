a8c598cc7e2965d6f64704c41aadcddbb7a33f12
Jason Logerquist <18403017+jay101pk@users.noreply.github.com>
2023-08-13 23:49:32 -0500

    Cleanup service file, split-out equipment component.

--- src/app/character/[id]/page.tsx
+++ src/app/character/[id]/page.tsx
@@ -1,98 +1,41 @@
-"use client"
-import * as Models from '@/util/models' 
-import {useEffect, useState} from "react";
-import Image from 'next/image'
+import Equipment from "@/components/equipment";
+import { getBasicCharacterInfo } from "@/util/service";
 
-export default function Page({params}: { params: { id: string } }) {
+export default async function Page({ params }: { params: { id: string } }) {
+  const characterBasicInfoResponse = await getBasicCharacterInfo(params.id);
 
-    const [character, setCharacter] = useState<Models.CharacterBasicInfo>();
-    const [loading, setLoading] = useState<boolean>(true);
+  const rows = [];
+  for (let i = 0; i < 7; i++) {
+    // note: we are adding a key prop here to allow react to uniquely identify each
+    // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
+    rows.push(<div className="h-7 rounded-full bg-gray-700 w-1/2 mb-1"></div>);
+  }
 
-    const fetchCharacterBasicInfo = async () => {
-        const characterBasicInfoResponse = await fetch(`/api/character/${params.id}`);
-        const characterBasicInfo : Models.CharacterBasicInfo = await characterBasicInfoResponse.json();
-        setLoading(false);
-        setCharacter(characterBasicInfo);
-    }
-
-    useEffect(() => { //runs once on page loadup, add fetch methods here
-        fetchCharacterBasicInfo();
-        fetchEquipped();
-    }, [])
-
-    const rows = [];
-    for (let i = 0; i < 7; i++) {
-        // note: we are adding a key prop here to allow react to uniquely identify each
-        // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-        rows.push(<div className="h-7 rounded-full bg-gray-700 w-1/2 mb-1"></div>);
-    }
-
-    const [equip, setEquip] = useState<Models.Equipped>();
-
-    const fetchEquipped = async () => {
-        const equippedResponse = await fetch(`/api/equipment/${params.id}`);
-        const equipped : Models.Equipped = await equippedResponse.json();
-        console.log(equipped);
-        setLoading(false);
-        setEquip(equipped);
-    }
-
-    return (<div className="container mx-auto max-w-2xl m-10">
-        <div className="text-2xl p-5">Basic Character Information</div>
-        <hr/>
-        {loading || character == undefined ? <div>
-            <div role="status" className="animate-pulse mb-5">
-                {rows}
-                <span className="sr-only">Loading...</span>
-            </div>
-
-        </div> : <div className="p-5">
-            <div>
-                Character Name: {character.characterName}
-            </div>
-            <div>
-                Character Level: {character.level}
-            </div>
-            <div>
-                Job Name: {character.jobName}
-            </div>
-            <div>
-                Advancement: {character.jobGrowName}
-            </div>
-            <div>
-                Account Name: {character.adventureName}
-            </div>
-            <div>
-                Guild Name: {character.guildName}
-            </div>
+  return (
+    <div className="container mx-auto max-w-2xl m-10">
+      <div className="text-2xl p-5">Basic Character Information</div>
+      <hr />
+      {characterBasicInfoResponse == undefined ? (
+        <div>
+          <div role="status" className="animate-pulse mb-5">
+            {rows}
+            <span className="sr-only">Loading...</span>
+          </div>
         </div>
-        }
-
-        <div className="text-2xl p-5">Equipment</div>
-        {loading || equip == undefined ? <div>
-            <div role="status" className="animate-pulse mb-5">
-                {rows}
-                <span className="sr-only">Loading...</span>
-            </div>
-
-        </div> : <div className="p-5">
-            <div>
-            {equip.equipment.map((equip, index) => (
-                              <div key={index} className="px-2 py-2 bg-slate-500">
-                                <Image
-                                    src={'https://img-api.dfoneople.com/df/items/' + equip.itemId}
-                                    width={28}
-                                    height={28}
-                                    unoptimized={true}
-                                    alt ="item image"
-                                />
-                                {
-                                `${equip.slotName}:     ${equip.itemName} `
-                                } 
-                              </div>
-                          ))}
-            </div>
+      ) : (
+        <div className="p-5">
+          <div>Character Name: {characterBasicInfoResponse.characterName}</div>
+          <div>Character Level: {characterBasicInfoResponse.level}</div>
+          <div>Job Name: {characterBasicInfoResponse.jobName}</div>
+          <div>Advancement: {characterBasicInfoResponse.jobGrowName}</div>
+          <div>Account Name: {characterBasicInfoResponse.adventureName}</div>
+          <div>Guild Name: {characterBasicInfoResponse.guildName}</div>
         </div>
-        }
-    </div>);
+      )}
+
+      <div className="text-2xl p-5">Equipment</div>
+
+      <Equipment charId={params.id} />
+    </div>
+  );
 }
--- src/util/service.ts
+++ src/util/service.ts
@@ -1,320 +1,277 @@
-import * as Models from '@/util/models' 
+import * as Models from "@/util/models";
 
 const SERVER = "cain";
-const API_URL = "https://api.dfoneople.com/df/" + SERVER;
-const API_KEY = 'apikey=' + process.env.API_KEY;
+const API_URL = "https://api.dfoneople.com/df/servers/" + SERVER;
+
+const DEFAULT_HEADER: [string, string][] = [
+  ["Content-Type", "application/json"],
+  ["Accpet", " application/json"],
+  ["apikey", process.env.API_KEY ?? ""],
+];
 
 // 02. Get character ID by name API call
-export async function getCharacter(charName: string): Promise<Models.Characters> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getCharacter(
+  charName: string
+): Promise<Models.Characters> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  const request: RequestInfo = new Request("https://api.dfoneople.com/df/servers/cain/characters?characterName=" + charName + "&wordType=match&" + API_KEY, {
-    method: 'GET',
-    headers: headers
-  })
+  const request: RequestInfo = new Request(
+    `${API_URL}/characters?characterName=${charName}&wordType=match`,
+    {
+      method: "GET",
+      headers: DEFAULT_HEADER,
+    }
+  );
 
   // Use fetch to retrieve data
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 
 // 02. Get character ID by name API call
-export async function searchCharacter(searchText: string, rows?: number): Promise<Models.Characters> {
-  if (searchText.length < 4) return new Promise<Models.Characters>((resolve) => { resolve({rows: []}) });
+export async function searchCharacter(
+  searchText: string,
+  rows?: number
+): Promise<Models.Characters> {
+  if (searchText.length < 4)
+    return new Promise<Models.Characters>((resolve) => {
+      resolve({ rows: [] });
+    });
 
-  const headers: Headers = new Headers();
-  headers.set('Content-Type', 'application/json');
-  headers.set('Accept', 'application/json');
-  
-  var searchURI = `https://api.dfoneople.com/df/servers/cain/characters?characterName=${searchText}&wordType=full&limit=${rows ?? 10}&${API_KEY}`;
+  var searchURI = `${API_URL}/characters?characterName=${searchText}&wordType=full&limit=${
+    rows ?? 10
+  }`;
 
   const request: RequestInfo = new Request(searchURI, {
-    method: 'GET',
-    headers: headers
+    method: "GET",
+    headers: DEFAULT_HEADER,
   });
-  
+
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 
 // 03. Get basic info API Call
-export async function getBasicCharacterInfo(charId: string): Promise<Models.CharacterBasicInfo> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-  headers.set('apikey', process.env.API_KEY??'')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getBasicCharacterInfo(
+  charId: string
+): Promise<Models.CharacterBasicInfo> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  const request: RequestInfo = new Request(`https://api.dfoneople.com/df/servers/cain/characters/${charId}`, {
-    method: 'GET',
-    headers: headers
-  })
+  const request: RequestInfo = new Request(`${API_URL}/characters/${charId}`, {
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   // Use fetch to retrieve data
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
 
   return res.json();
 }
 
 // 04. Getting stat info API call
-export async function getStats(charID: string): Promise<Models.Stat> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-  headers.set('apikey', process.env.API_KEY??'')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getStats(charId: string): Promise<Models.Stat> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var Uri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/status?`;
+  var Uri = `${API_URL}/characters/${charId}/status?`;
   const request: RequestInfo = new Request(Uri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 05. Getting equipment info API call
-export async function getEquipment(charID: string): Promise<Models.Equipped> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-  headers.set('apikey', process.env.API_KEY??'')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getEquipment(charId: string): Promise<Models.Equipped> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var Uri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/equip/equipment?`;
+  var Uri = `${API_URL}/characters/${charId}/equip/equipment?`;
   const request: RequestInfo = new Request(Uri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 06. Getting avatar info API call
-export async function getAvatar(charID: string): Promise<Models.Avatars> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getAvatar(charId: string): Promise<Models.Avatars> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var avatarUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/equip/avatar?${API_KEY}`;
+  var avatarUri = `${API_URL}/characters/${charId}/equip/avatar`;
   const request: RequestInfo = new Request(avatarUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 07. Getting creature API call
-export async function getCreature(charID: string): Promise<Models.Creatures> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getCreature(charId: string): Promise<Models.Creatures> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var creatureUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/equip/creature?${API_KEY}`;
+  var creatureUri = `${API_URL}/characters/${charId}/equip/creature?`;
   const request: RequestInfo = new Request(creatureUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 08. Getting Insignia API call
-export async function getInsignia(charID: string): Promise<Models.Insignia> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getInsignia(charId: string): Promise<Models.Insignia> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var flagUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/equip/flag?${API_KEY}`;
+  var flagUri = `${API_URL}/characters/${charId}/equip/flag?`;
   const request: RequestInfo = new Request(flagUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 09. Getting Talismans API call
-export async function getTalisman(charID: string): Promise<Models.Talismans> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getTalisman(charId: string): Promise<Models.Talismans> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var talismanUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/equip/talisman?${API_KEY}`;
+  var talismanUri = `${API_URL}/characters/${charId}/equip/talisman?`;
   const request: RequestInfo = new Request(talismanUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 10. Gettings skills API call
-export async function getSkill(charID: string): Promise<Models.Skills> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getSkill(charId: string): Promise<Models.Skills> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var skillUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/skill/style?${API_KEY}`;
+  var skillUri = `${API_URL}/characters/${charId}/skill/style?`;
   const request: RequestInfo = new Request(skillUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 11. Getting buff swap equipment API call
-export async function getBuffEquip(charID: string): Promise<Models.buffEquips> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getBuffEquip(charId: string): Promise<Models.buffEquips> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var BuffEquipUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/skill/buff/equip/equipment?${API_KEY}`;
+  var BuffEquipUri = `${API_URL}/characters/${charId}/skill/buff/equip/equipment?`;
   const request: RequestInfo = new Request(BuffEquipUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 12. Getting buff avatar API call
-export async function getBuffAvatar(charID: string): Promise<Models.buffAvatars> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getBuffAvatar(
+  charId: string
+): Promise<Models.buffAvatars> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var BuffAvaUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/skill/buff/equip/avatar?${API_KEY}`;
+  var BuffAvaUri = `${API_URL}/characters/${charId}/skill/buff/equip/avatar?`;
   const request: RequestInfo = new Request(BuffAvaUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
 
 // 13. Getting buff swap creatures API call
-export async function getBuffCreature(charID: string): Promise<Models.buffCreatures> {
-
-  const headers: Headers = new Headers()
-  headers.set('Content-Type', 'application/json')
-  headers.set('Accept', 'application/json')
-
-  // Create the request object, which will be a RequestInfo type. 
+export async function getBuffCreature(
+  charId: string
+): Promise<Models.buffCreatures> {
+  // Create the request object, which will be a RequestInfo type.
   // Here, we will pass in the URL as well as the options object as parameters.
-  var buffCreUri = `https://api.dfoneople.com/df/servers/cain/characters/${charID}/skill/buff/equip/creature?${API_KEY}`;
+  var buffCreUri = `${API_URL}/characters/${charId}/skill/buff/equip/creature?`;
   const request: RequestInfo = new Request(buffCreUri, {
-    method: 'GET',
-    headers: headers
-  })
+    method: "GET",
+    headers: DEFAULT_HEADER,
+  });
 
   const res = await fetch(request);
 
   if (!res.ok) {
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
-  // Use fetch to retrieve data 
+  // Use fetch to retrieve data
   return res.json();
 }
