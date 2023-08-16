c236714790a61facd7946811e6737f97fd20f63b
NGoffi <92411234+NGoffi@users.noreply.github.com>
2023-08-16 04:06:58 -0500

    added more features to equipment. added styling

--- src/components/equipment.tsx
+++ src/components/equipment.tsx
@@ -9,23 +9,16 @@ async function Options({option}: {option: Models.Option}) {
 // basic 105 gear options
   const damage = option.damage
   return (
-    <div>
-      <br></br>
-      <div style= {{
-        backgroundColor: 'black',
-      }}>
-      <h1 style= {{color: 'white', fontSize: 10}} >{`Level: ${option.level}`}</h1>
+    <div className = 'border m-0.5 p-1'>
+      <div>
+      <h1 style= {{color: 'white'}} >{`Level: ${option.level}`}</h1>
       </div>
-      <div style= {{
-        backgroundColor: 'gray', fontSize: 10
-      }}>
+      <div>
       {`Damage Value:`}
       <span style ={{color: 'blue'}}>{`${damage} `}</span>
       {`Buff: ${option.buff}`}
       </div>
-      <div style= {{
-        backgroundColor: 'white', fontSize: 10
-      }}>
+      <div>
       {`${option.explainDetail}`}
       </div>
     </div>
@@ -79,42 +72,35 @@ async function Option5({option5}: {option5: Models.Option5}) {
 async function Status({status}: {status: Models.Status}) {
 // enchants
   return (
-    <div>
+    <div style = {{
+      color: `#90ff90`
+      }}>
       {`${status.name} `}
       {`${status.value} `}
     </div>
   )
 }
 
-async function Itemimage({itemimage}: {itemimage: Models.Equipment}) {
+async function FusionImage({fusionimage}: {fusionimage: Models.Equipment}) {
+  // returns image of fusion equipment
+  const rarityColors = new Map<string, string>([
+    ["Common",     "#FFFFFF"],
+    ["Uncommon",   "#68D5ED"],
+    ["Rare",     "#B36BFF"],
+    ["Unique",     "#FF00FF"],
+    ["Epic", "#FFB400"],
+    ["Chronicle", "#FF6666"],
+    ["Legendary", "#FF7800"],
+    ["Mythic", "#FFB400"],
+  ]);
   return (
-    <div className="equipmentImageContainer" style = {{
-      display: 'flex'
+    <div style ={{
     }}>
-      <div className ='equipmentBorderContainer' style = {{
-        position: 'absolute',
-        display: 'flex'
-      }}>
-      <Image
-      src={"/epic.png"}
-      width={28}
-      height={28}
-      unoptimized={true}
-      alt="item image"
-    />
-      </div>
-      <div>
-    <Image style = {{border: '1px outset #FFB400'}}
-      src={"https://img-api.dfoneople.com/df/items/" + itemimage.itemId}
-      width={28}
-      height={28}
-      unoptimized={true}
-      alt="item image"
-    />
-    </div>
-    {itemimage.upgradeInfo?.itemId != undefined &&
-      <Image
-        src={"https://img-api.dfoneople.com/df/items/" + itemimage.upgradeInfo?.itemId}
+    {fusionimage.upgradeInfo?.itemId != undefined &&
+      <Image style = {{
+        border: `1px outset ${rarityColors.get(fusionimage.itemRarity)}`,
+      }}
+        src={"https://img-api.dfoneople.com/df/items/" + fusionimage.upgradeInfo?.itemId}
         width={28}
         height={28}
         unoptimized={true}
@@ -125,9 +111,68 @@ async function Itemimage({itemimage}: {itemimage: Models.Equipment}) {
   )
 }
 
-async function Upgrade({upgrade}: {upgrade: Models.Equipment}) {
+async function BaseItemImage({baseitemimage}: {baseitemimage: Models.Equipment}) {
+  // returns image of base equipment
+  const rarityColors = new Map<string, string>([
+    ["Common",     "#FFFFFF"],
+    ["Uncommon",   "#68D5ED"],
+    ["Rare",     "#B36BFF"],
+    ["Unique",     "#FF00FF"],
+    ["Epic", "#FFB400"],
+    ["Chronicle", "#FF6666"],
+    ["Legendary", "#FF7800"],
+    ["Mythic", "#FFB400"],
+  ]);
   return (
+    <div style ={{
+    }}>
+      <Image style = {{
+        border: `1px outset ${rarityColors.get(baseitemimage.itemRarity)}`,
+      }}
+        src={"https://img-api.dfoneople.com/df/items/" + baseitemimage.itemId}
+        width={28}
+        height={28}
+        unoptimized={true}
+        alt="item image"
+      />
+    </div>
+  )
+}
+
+async function AllItemimage({itemimage}: {itemimage: Models.Equipment}) {
+  // returns both base and fusion equipment images
+  return (
+    <div style = {{
+      display: 'flex',
+    }}>
+    <div className="equipmentImageContainer m-1 p-1" style = {{
+      display: 'flex',
+    }}>
+      <div>
+      <BaseItemImage baseitemimage={itemimage}/>
+    </div>
+    </div>
+    <div className="equipmentImageContainer m-1 p-1" style = {{
+      display: 'flex',
+    }}>
     <div>
+      <FusionImage fusionimage={itemimage}/>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+async function Upgrade({upgrade}: {upgrade: Models.Equipment}) {
+  // returns the reinforcement or amplification of an item
+  var color = "#0096FF"
+  if (upgrade.amplificationName != undefined) {
+    color = "#FC0FC0"
+  }
+  return (
+    <div style = {{
+      color: `${color}`
+    }}>
     {upgrade.reinforce != 0 &&
     `+ ${upgrade.reinforce} `
     }
@@ -139,6 +184,7 @@ async function Upgrade({upgrade}: {upgrade: Models.Equipment}) {
 }
 
 async function Fusion({fusion}: {fusion: Models.Equipment}) {
+  // gets info on fusion equipment
   return (
     <div>
     { fusion.ispinsInfo != undefined &&
@@ -170,6 +216,7 @@ async function Fusion({fusion}: {fusion: Models.Equipment}) {
 }
 
 async function Total({total}: {total : Models.Total}) {
+  // basic information for 105 epic gear
   return (
     <div>  
     Total Option Level: {total.level}
@@ -177,14 +224,21 @@ async function Total({total}: {total : Models.Total}) {
     Total Damage Value: {total.damage} 
     <br></br>
     Total Buff Value: {total.buff}
+    <br></br>
+    <br></br>
+    Option Effects:
     </div>
   )
 }
 
 async function ItemInfo({itemInfo}: {itemInfo: Models.Equipment}) {
+  // returns general item info, applicable to nearly all items
   const itemid = await getItemInfo(itemInfo.itemId)
   return (
     <div>
+      <div>
+        General Stats:
+      </div>
     <div>
       {itemid.itemStatus.map((itemStatu,index) => (
         <ItemStatu key={index} itemStatu={itemStatu} />
@@ -197,7 +251,8 @@ async function ItemInfo({itemInfo}: {itemInfo: Models.Equipment}) {
 }
 
 async function ItemStatu({itemStatu}: {itemStatu: Models.ItemStatu}) {
-  if (itemStatu.name !== 'Durability') {
+  // returns specific general item info
+  if ((itemStatu.name !== 'Durability') && (itemStatu.name !== 'Adventurer Fame') && (itemStatu.name !== 'Attack attribute')) {
     return (
       <div>
         {`${itemStatu.name}: ${itemStatu.value}`}
@@ -207,15 +262,36 @@ async function ItemStatu({itemStatu}: {itemStatu: Models.ItemStatu}) {
 }
 
 async function Basic({basic}: {basic: Models.Equipment}) {
+  // returns html displaying basic gear information
+  const rarityColors = new Map<string, string>([
+    ["Common",     "#FFFFFF"],
+    ["Uncommon",   "#68D5ED"],
+    ["Rare",     "#B36BFF"],
+    ["Unique",     "#FF00FF"],
+    ["Epic", "#FFB400"],
+    ["Chronicle", "#FF6666"],
+    ["Legendary", "#FF7800"],
+    ["Mythic", "#FFB400"],
+  ]);
   return (
     <div>
-      {`${basic.slotName}: ${basic.itemName}`}
       <div>
-        <Itemimage itemimage={basic} />
+      <BaseItemImage baseitemimage={basic}/>
+      </div>
+      <div style = {{
+        color: rarityColors.get(`${basic.itemRarity}`)
+      }}>
+        {`${basic.itemRarity} ${basic.itemTypeDetail}`}
+        </div>
+      <div className = "xxx"style = {{
+        color: rarityColors.get(`${basic.itemRarity}`)
+      }}>
+      {`${basic.itemName}`}
       </div>
       <div>
         <Upgrade upgrade={basic} />
       </div>
+      <br></br>
       <div>
         {basic.growInfo?.total != undefined &&
           <Total total={basic.growInfo.total} />
@@ -226,7 +302,9 @@ async function Basic({basic}: {basic: Models.Equipment}) {
       </div>
       <br></br>
 
-      <div>
+      <div style = {{
+      color: `#00dc00`
+      }}>
         { basic.enchant != undefined &&
          `Enchants:`
         }
@@ -236,38 +314,72 @@ async function Basic({basic}: {basic: Models.Equipment}) {
         ))}
       <div>
         <br></br>
-
-        <div>
-          {basic.itemRarity}
-        </div>
       </div>
     </div>
   )
 }
 
 async function FusionCheck({fusionCheck}: {fusionCheck: Models.Equipment}) {
-  return (
-    <Fusion fusion={fusionCheck}/>
-  )
+  // checks if a piece has a fusion and returns information on the fusion equipment
+  if (fusionCheck.upgradeInfo != undefined) {
+    return (
+      <div>
+        <div>
+          <FusionImage fusionimage={fusionCheck}/>
+        </div>
+      <div className="text-[#FFB400]">
+        {fusionCheck.upgradeInfo.itemName}
+      </div>
+      <Fusion fusion={fusionCheck}/>
+      </div>
+    )
+  }
+  else if(fusionCheck.itemTypeDetail == "Title") {
+    return (
+      <div>
+        Some title images may be broken at this time. Other information should be accurate
+      </div>
+    )
+  }
+  else {
+    return(
+      <div>
+        This equipment currently has no Fusion
+      </div>
+    )
+  }
+
 }
 
 async function Item({equipItem}: {equipItem : Models.Equipment}) {
+  // creates a 'card' for each piece of equipment a character is wearing
   return (
-    <div className= "grid grid-cols-3 gap-4 px-4 py-2 bg-[#000000] text-red-500 rounded-3xl m-2 font-serif">
-      <div>
-        <ItemInfo itemInfo={equipItem}/>
-      </div>
-      <div>
-      <Basic basic={equipItem}/>
+    <div>
+    <div>
+      {equipItem.slotName}
     </div>
-      <div>
+    <div>
+    <AllItemimage itemimage={equipItem} />
+    </div>
+    <div className= "grid grid-cols-3 gap-4 px-4 py-2 bg-[#000000] text-red-500 rounded-3xl m-1 p-1"style = {{
+      fontSize: 10
+    }}>
+      <div className= "border m-1 p-2">
+      <Basic basic={equipItem}/>        
+      </div>
+      <div className= "border m-1 p-2">
+      <ItemInfo itemInfo={equipItem}/>
+    </div>
+      <div className= "border m-1 p-2">
       <FusionCheck fusionCheck={equipItem}/>
+    </div>
     </div>
     </div>
   );
 }
 
 export default async function Equipment({ charId }: { charId: string }) {
+  // maps equipment
   const equip = await getEquipment(charId);
   return (
     <div className="p-5">
