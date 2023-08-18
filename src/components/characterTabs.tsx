727ab8516d347a441a1fdd1632702334538392f6
Jason Logerquist <18403017+jay101pk@users.noreply.github.com>
2023-08-18 01:04:12 -0500

    Make a simple tab component for character info

---
+++ src/components/characterTabs.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { Tabs } from "flowbite-react";
+import Equipment from "./equipment";
+import React from "react";
+
+export default function CharacterTabs({
+  charId,
+  stats
+}: {
+    charId: string;
+    stats: React.ReactNode;
+}) {
+  return (
+    <>
+      <Tabs.Group aria-label="Defatult tabs" style="default">
+        <Tabs.Item active title="stats">
+          {stats}
+        </Tabs.Item>
+        <Tabs.Item title="equipment">
+            <Equipment charId={charId}/>
+        </Tabs.Item>
+        <Tabs.Item title="other">
+          <p>test</p>
+        </Tabs.Item>
+      </Tabs.Group>
+    </>
+  );
+}
